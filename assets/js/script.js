095bf4735cec93475342bff68b848c33616c20e3
Sergio Vidales Perez <srvidales@gmail.com>
2023-07-13 00:40:31 -0700

    Adds quiz step vars to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
-console.log('Hello, world!');
+// Header Section
+var headerEl = document.querySelector('header');
+var headerHighScoresEl = document.getElementById('high-scores-link');
+var headerTimeLeftEl = document.getElementById('time-left-span');
+
+// Intro Step
+var introStepEl = document.getElementById('intro-section');
+var introStartQuizEl = document.getElementById('start-quiz-button');
+
+// Quiz Step
+var quizStepEl = document.getElementById('quiz-section');
+var quizQuestionEl = document.getElementById('question-heading');
+var quizAnswerEls = document.getElementsByClassName('answer-button');
+
+// Summary Step
+var summaryStepEl = document.getElementById('summary-section');
+var summaryFinalScoreEl = document.getElementById('final-score-span');
+
+// Evaluation Section
+var evaluationEl = document.getElementById('evaluation-div');
+var evaluationResultEl = document.getElementById('evaluation-paragraph');
+
+// High Scores Step
+var highScoresStepEl = document.getElementById('high-scores-section');
+var highScoresListEl = document.getElementById('high-scores-list');
+var highScoresBackEl = document.getElementById('back-button');
+var highScoresClearEl = document.getElementById('clear-high-scores-button');
